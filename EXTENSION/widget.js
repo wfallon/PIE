b99c9be25ac8217ed76f554bfe1e07ec4e8f613e
Brendan <braimann2017@pingry.org>
2016-04-12 17:20:36 -0400

    Added working time

---
+++ EXTENSION/widget.js
@@ -0,0 +1,37 @@
+/*
+*	Brendan Raimann and Billy Fallon
+*	Controls:
+*		Time
+*		Date
+*		Weather
+*/
+
+$(document).ready(function(){
+	
+	/*
+	*	Helper function for the time. Adds a zero in front of the number if necessary.
+	*/
+	function addZero(x) {
+		if (x < 10)
+			return "0" + x;
+		return x;
+	}
+	/*
+	*	Recursive method to keep time.
+	*/
+	function startTime() {
+		var today = new Date();
+		if (today.getHours() <= 12)
+			h = today.getHours();
+		else
+			h = today.getHours() - 12;
+        m = addZero(today.getMinutes());
+		s = addZero(today.getSeconds());
+		document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+		setTimeout(function(){ startTime(); }, 1000);
+	}
+	
+	startTime();
+	
+	
+});
