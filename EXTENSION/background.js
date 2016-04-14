be3d5ce88943440fefa2591b9eed9b5245a15a69
Billy Fallon <wfallon@pingry.org>
2016-04-14 11:46:42 -0400

    4/14 In Class
    Twitter and fade complete.

--- EXTENSION/background.js
+++ EXTENSION/background.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
 	$(document.body).css("background-image", "url('photos/Image" + (~~(Math.random() * 7) + 1) + ".jpg')");
 	$(document.body).css("background-size", "cover");
+
+	$(".stage1").fadeOut(2000);
 });
