35d1edb68d9c4251d6f4693e9f9a0c5acc9f1ab1
Brendan <braimann2017@pingry.org>
2016-04-26 08:42:44 -0400

    Added Shadows
    Time and Date now have light shadows that transition as the page loads

--- EXTENSION/background.js
+++ EXTENSION/background.js
@@ -4,5 +4,18 @@ $(document).ready(function(){
 
 
 	$(".stage1").fadeOut(2000);
+	
+	/**
+	*	Fading in shadows for the Time and Date
+	*	*Fixes a loading glitch*
+	*/
+	
+	window.setTimeout(function(){$(document.getElementById("time")).css("text-shadow", "0px 0px 15px hsla(120, 100%, 0%, .05)")},500);
+	window.setTimeout(function(){$(document.getElementById("time")).css("text-shadow", "0px 0px 15px hsla(120, 100%, 0%, .1)")},1000);
+	window.setTimeout(function(){$(document.getElementById("time")).css("text-shadow", "0px 0px 15px hsla(120, 100%, 0%, .15)")},1500);
+	
+	window.setTimeout(function(){$(document.getElementById("date")).css("text-shadow", "0px 0px 15px hsla(120, 100%, 0%, .05)")},500);
+	window.setTimeout(function(){$(document.getElementById("date")).css("text-shadow", "0px 0px 15px hsla(120, 100%, 0%, .1)")},1000);
+	window.setTimeout(function(){$(document.getElementById("date")).css("text-shadow", "0px 0px 15px hsla(120, 100%, 0%, .2)")},1500);
 });
 
